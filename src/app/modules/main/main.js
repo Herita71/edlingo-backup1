e33b3facee10d3de4775ace5fd65dee3a8997557
Andrew Xie <andyxie@Andrews-MacBook-Air.local>
2017-02-07 00:37:35 -0800

    navbar

--- src/app/modules/main/main.js
+++ src/app/modules/main/main.js
@@ -1,10 +1,12 @@
 (function(){
     'use strict';
 
-    AppComponent.$inject = ['config'];
-    function AppComponent(config){
+    AppComponent.$inject = ['config', '$scope', '$state'];
+    function AppComponent(config, $scope, $state){
         var that = this;
         that.title = config.name;
+
+        $scope.config = config;
     };
 
     angular.module('app.main')
--- src/app/modules/main/main.module.js
+++ src/app/modules/main/main.module.js
@@ -1,6 +1,18 @@
 (function(){
     'use strict';
+    appConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function appConfig($stateProvider, $urlRouterProvider) {
+        $stateProvider
+        .state('app', {
+            url: '/app',
+            templateUrl: 'app/modules/main/app.html'
+        });
+
+        $urlRouterProvider.otherwise('/app');
+    }
     angular.module('app.main', [
         //Imports
-    ]);
+        'ui.router'
+    ])
+    .config(appConfig);
 })();
